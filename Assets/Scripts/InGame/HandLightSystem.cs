ced0157391be9f34a72140b9c436ff7b99256f1e
JangGwon <tlsakch510@naver.com>
2022-08-14 01:36:52 +0900

    경고효과 추가 및 코드 리펙토링

--- Assets/Scripts/InGame/HandLightSystem.cs
+++ Assets/Scripts/InGame/HandLightSystem.cs
@@ -3,13 +3,28 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
+public enum Dark_lv
+{
+    none,
+    _1lv,
+    _2lv
+}
+
 public class HandLightSystem : MonoBehaviour
 {
     public float Max_handlight_time = 30f;
     public float handlight_lifetime = 30f;
     public float warning_time = 15f;
-    Image handlight_img;
     float handlightTimer_percentage = 1f;
+
+    public InGameManeger inGameManeger_cs;
+
+    public GameObject edge_dark_obj;
+    public Image edge_dark_img;
+    Image handlight_img;
+
+    Dark_lv dark_lv = Dark_lv.none;
+
     void Start()
     {
         handlight_img = GetComponent<Image>();
@@ -17,17 +32,34 @@ public class HandLightSystem : MonoBehaviour
 
     void Update()
     {
-        handlight_lifetime -= Time.deltaTime;
-        handlight_img.fillAmount = handlight_lifetime / Max_handlight_time;
-
-
-        if (handlight_lifetime == 0)        // 끝났음
+        //if (inGameManeger_cs.gameState == GameState.playingInGame)
         {
-
+            handlight_lifetime -= Time.deltaTime;
+            handlight_img.fillAmount = handlight_lifetime / Max_handlight_time;
+            if (handlight_lifetime < 28 && dark_lv == Dark_lv.none)
+            {
+                dark_lv = Dark_lv._1lv;
+                StartCoroutine(CircleFadeIn_step1());
+            }
+            if (handlight_lifetime == 0)        // 끝났음
+            {
+                inGameManeger_cs.gameState = GameState.timeout;
+            }
         }
     }
     public void Get_handlightbettery()
     {
         handlight_lifetime += 30;
+    }
+    
+    IEnumerator CircleFadeIn_step1()           // 1단계
+    {
+        edge_dark_obj.SetActive(true);
+        edge_dark_obj.transform.localScale = new Vector3(2.6f, 2.3f, 1);
+        for (int i = 0; i <= 20; i++)
+        {
+            edge_dark_obj.transform.localScale = new Vector3(2.6f - (0.06f * i), 2.3f - (0.05f * i), 1f);
+            yield return new WaitForSeconds(0.05f);
+        }
     }
 }
