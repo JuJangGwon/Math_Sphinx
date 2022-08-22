9812c2a86485320d3d27ade3c7cbf89d94e6564d
JangGwon <tlsakch510@naver.com>
2022-08-22 15:37:37 +0900

    정답 발판 추가

--- Assets/Scripts/InGame/Character/Character_Collider.cs
+++ Assets/Scripts/InGame/Character/Character_Collider.cs
@@ -6,12 +6,28 @@ public class Character_Collider : MonoBehaviour
 {
     public GameObject Problem_popup;
 
-    void OnTriggerEnter(Collider other)
+    void OnTriggerEnter2D(Collider2D other)
     {
         if (other.gameObject.tag == "ABC")
         {
-            print("###");
             Problem_popup.SetActive(true);
+        }
+        //
+        if (other.gameObject.tag == "AnswerFootBoard1")
+        {
+            Debug.Log("1번 정답 발판 밟음");
+        }
+        else if (other.gameObject.tag == "AnswerFootBoard2")
+        {
+            Debug.Log("2번 정답 발판 밟음");
+        }
+        else if (other.gameObject.tag == "AnswerFootBoard3")
+        {
+            Debug.Log("3번 정답 발판 밟음");
+        }
+        else if (other.gameObject.tag == "AnswerFootBoard4")
+        {
+            Debug.Log("4번 정답 발판 밟음");
         }
     }
 }
