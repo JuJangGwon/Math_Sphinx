d3361a3c807376fce263d7d210f6a2816009baeb
JangGwon <tlsakch510@naver.com>
2022-08-23 03:19:21 +0900

    정답 발판 스크립팅

---
+++ Assets/Scripts/InGame/FindAnswerWay.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FindAnswerWay : MonoBehaviour
+{
+    public GameObject go;
+    public int Answer = 0;
+
+    public void PlayerStart(int num)
+    {
+
+    }
+
+    void CreateProblem()
+    {
+        Answer = 1;
+    }
+    
+}
