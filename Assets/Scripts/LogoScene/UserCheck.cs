f9b44f95b40eb57404231a42fce9fe203c6d2a53
JeonSeaStar <88014706+JeonSeaStar@users.noreply.github.com>
2022-11-03 12:38:39 +0900

    로고 씬 ui 연결

--- Assets/Scripts/LogoScene/UserCheck.cs
+++ Assets/Scripts/LogoScene/UserCheck.cs
@@ -10,6 +10,9 @@ using TMPro;
 
 public class UserCheck : MonoBehaviour
 {
+    [Header("애니메이터")]
+    public Animator anime;
+
     [Header("로그인 팝업")]
     public GameObject login_popup;
     public TextMeshProUGUI login_id;
@@ -31,6 +34,7 @@ public class UserCheck : MonoBehaviour
     public GameObject Information_popup;
     public TextMeshProUGUI information_title;
     public TextMeshProUGUI information_content;
+    public Button btn;
     public string[] title_text;
     public string[] content_text;
 
@@ -83,20 +87,65 @@ public class UserCheck : MonoBehaviour
         //context = new DynamoDBContext(DBclient);
     }
 
-    public void Open_Popup(GameObject pu, int n)
+    public void Open_Information_Popup(int n)
     {
         switch (n)
         {
             case 0:
+                information_title.text = title_text[n];
+                information_content.text = content_text[n];
+                btn.onClick.RemoveAllListeners();
+                btn.onClick.AddListener(Close_Nickname_Complete);
+                Information_popup.SetActive(true);
+                break;
             case 1:
             case 2:
                 information_title.text = title_text[n];
+                information_content.text = content_text[n];
+                btn.onClick.RemoveAllListeners();
+                btn.onClick.AddListener(delegate { Close_Popup(Information_popup); });
+                Information_popup.SetActive(true);
                 break;
-            default:
+            case 3:
+                login_popup.SetActive(true);
+                break;
+            case 4:
+                signup_popup.SetActive(true);
+                break;
+            case 5:
+                nickname_popup.SetActive(true);
                 break;
         }
+    }
 
-        pu.SetActive(true);
+    public void Open_Sign_Up()
+    {
+        Close_Popup(login_popup);
+        Open_Information_Popup(4);
+    }
+
+    public void Close_Sign_Up()
+    {
+        Close_Popup(signup_popup);
+        Open_Information_Popup(3);
+    }
+
+    public void Sign_Up_Check()
+    {
+        Close_Popup(signup_popup);
+        Open_Information_Popup(5);
+    }
+
+    public void Nickname_Check()
+    {
+        Close_Popup(nickname_popup);
+        Open_Information_Popup(0);
+    }
+
+    public void Close_Nickname_Complete()
+    {
+        Close_Popup(Information_popup);
+        Open_Information_Popup(3);
     }
 
     public void Close_Popup(GameObject pu)
