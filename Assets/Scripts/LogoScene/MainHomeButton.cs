f7358a94650ca6d542ba1d5936a4b560b7528f07
JangGwon <tlsakch510@naver.com>
2022-11-11 22:44:10 +0900

    플레이시간 80초 -> 100초 로 변경

--- Assets/Scripts/LogoScene/MainHomeButton.cs
+++ Assets/Scripts/LogoScene/MainHomeButton.cs
@@ -12,8 +12,8 @@ public class MainHomeButton : MonoBehaviour
             case 1:     // 탐험하기버튼
                 PlayerPrefs.SetInt("pos", 1);
                 PlayerPrefs.SetInt("newgame", 1);
-                PlayerPrefs.SetFloat("max_handlight", 80f);
-                PlayerPrefs.SetFloat("now_handlgiht", 80f);
+                PlayerPrefs.SetFloat("max_handlight", 100f);
+                PlayerPrefs.SetFloat("now_handlgiht", 100f);
                 SceneManager.LoadScene("InGameScene");
                 break;
             case 2:
